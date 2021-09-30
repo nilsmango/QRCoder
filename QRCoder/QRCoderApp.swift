1bebf5fc77a125dc47456a415e9c82aee81a876a
Simon Lang <simonmilagrolang@icloud.com>
2021-09-30 20:53:46 +0100

    Initial Commit

---
+++ QRCoder/QRCoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCoderApp.swift
+//  QRCoder
+//
+//  Created by Simon Lang on 30.09.21.
+//
+
+import SwiftUI
+
+@main
+struct QRCoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
